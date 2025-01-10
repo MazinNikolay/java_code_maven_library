01ef4b01dd8694b25d827dd13d1eeea22413486b
kolia <koliamba_85@mail.ru>
2025-01-10 13:48:00 +0800

    Build test

--- GeometryUtils/src/test/java/com/javacode/learn/AppTest.java
+++ GeometryUtils/src/test/java/com/javacode/learn/AppTest.java
@@ -1,11 +1,17 @@
 package com.javacode.learn;
 
 import org.junit.jupiter.api.Test;
+import com.javacode.learn.model.impl.Triangle;
+import com.javacode.learn.model.Shape;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
 
     @Test
     void isEqualShapes() {
-
+        Shape shape1 = new Triangle(3.0, 4.0, 4.0);
+        Shape shape2 = new Triangle(4.0, 6.0, 7.0);
+        assertEquals(App.isEqualShapes(shape1, shape2), true);
     }
 }
